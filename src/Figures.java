3af065bddb04738b07e3145b411163d89e73c7bc
Pavel Popov <legeon48@mail.ru>
2022-10-14 11:03:47 +0300

    изменено

--- src/Figures.java
+++ src/Figures.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Figures {
+public class Figures {
+    public double rectangleArea(double length,double width){
+        return length * width;
+    }
+    public double squareArea(double length){
+        return Math.pow(length,2);
+    }
+    public double triangleArea(double height,double footing){
+        return 0.5 * height * footing;
+    }
+    public double diamondArea(double diagonal1, double diagonal2){
+        return diagonal1 * diagonal2 / 2;
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Test {
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test {
+    private Figures figure;
+
+    @Before
+    public void setUp(){
+        figure=new Figures();
+    }
+
+    @After
+    public void tearDown(){
+        figure = null;
+    }
+
+    @org.junit.Test
+    public void testRectang(){
+        double result = figure.rectangleArea(2,5);
+        assertEquals(10,result,0);
+    }
+    @org.junit.Test
+    public void testSquare(){
+        double result = figure.squareArea(2);
+        assertEquals(4,result,0);
+    }
+    @org.junit.Test
+    public void testTriangle(){
+        double result = figure.triangleArea(5,20);
+        assertEquals(50,result,0);
+    }
+    @org.junit.Test
+    public void testDiamond(){
+        double result = figure.diamondArea(10,10);
+        assertEquals(50,result,0);
+    }
 }
